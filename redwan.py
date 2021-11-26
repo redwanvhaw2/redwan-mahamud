c25feb2df82d80e673c1cf9499577421d925456e
redwanvhaw2 <95042693+redwanvhaw2@users.noreply.github.com>
2021-11-26 16:36:06 +0600

    redwan.py

---
+++ redwan.py
@@ -0,0 +1,107 @@
+{
+
+  "name": "footballcli",
+
+  "version": "0.14.10",
+
+  "description": "Command Line Interface for next fixtures, live scores and more",
+
+  "main": "cli.js",
+
+  "scripts": {
+
+    "lint": "eslint ./cli.js ./cmds/*.js"
+
+  },
+
+  "bin": {
+
+    "football": "./cli.js"
+
+  },
+
+  "engines": {
+
+    "node": ">=7.10.1"
+
+  },
+
+  "repository": {
+
+    "type": "git",
+
+    "url": "git+https://github.com/manrajgrover/football-cli.git"
+
+  },
+
+  "keywords": [
+
+    "football",
+
+    "scores",
+
+    "live",
+
+    "fixtures",
+
+    "cli"
+
+  ],
+
+  "author": "Manraj Singh <manrajsinghgrover@gmail.com> (http://manrajsingh.in)",
+
+  "license": "MIT",
+
+  "bugs": {
+
+    "url": "https://github.com/manrajgrover/football-cli/issues"
+
+  },
+
+  "homepage": "https://github.com/manrajgrover/football-cli#readme",
+
+  "dependencies": {
+
+    "chalk": "^1.1.3",
+
+    "cli-table3": "^0.5.0",
+
+    "inquirer": "^1.1.2",
+
+    "jsonexport": "^2.2.0",
+
+    "mkdirp": "^0.5.1",
+
+    "moment": "^2.19.3",
+
+    "ora": "^0.3.0",
+
+    "request": "^2.87.0",
+
+    "yargs": "^5.0.0"
+
+  },
+
+  "devDependencies": {
+
+    "eslint": "^5.3.0",
+
+    "eslint-config-airbnb": "17.1.0",
+
+    "eslint-config-prettier": "^3.1.0",
+
+    "eslint-plugin-import": "^2.14.0",
+
+    "eslint-plugin-jsx-a11y": "^6.1.1",
+
+    "eslint-plugin-prettier": "^3.0.0",
+
+    "eslint-plugin-react": "^7.11.0",
+
+    "prettier": "^1.14.3"
+
+  }
+
+}
+
+ 
